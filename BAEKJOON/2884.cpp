46e1c644520bd9ab6eb671416d7a50941c43062d
ehdbs28 <doyun5373@naver.com>
2022-09-04 21:01:12 +0900

    백준 2884번
    https://www.acmicpc.net/problem/2884

---
+++ BAEKJOON/2884.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int h, m;
+    cin >> h >> m;
+
+    if(h == 0) h = 24;
+
+    m += h * 60;
+    m -= 45;
+
+    h = 0;
+    while(m >= 60){
+        m -= 60;
+        h++;
+    }
+
+    if()
+
+    cout << h << " " << m;
+}
