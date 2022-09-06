c1d691743892205261d6ef9783eaafb17a310a9c
ehdbs28 <doyun5373@naver.com>
2022-09-06 09:03:33 +0900

    백준 11021, 11022번
    a + b

--- BAEKJOON/11022.cpp
+++ BAEKJOON/11022.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main(){
-    int cnt = 0;
+    int cnt;
     cin >> cnt;
 
     for(int i = 0; i < cnt; i++){
         int a, b;
         cin >> a >> b;
-        printf("Case #%d: %d\n", i + 1, a + b);
+        printf("Case #%d: %d + %d = %d\n", i+1, a, b, a+b);
     }
 }
