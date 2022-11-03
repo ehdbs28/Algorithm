043a6f5c8f60d9d10c384300e608c2d6fe7cd668
ehdbs28 <doyun5373@naver.com>
2022-11-03 22:19:00 +0900

    test Commit

--- BAEKJOON/Data_Structure/1966_Printer Que.cpp
+++ BAEKJOON/Data_Structure/1966_Printer Que.cpp
@@ -1,29 +1,48 @@
 #include<iostream>
-#include<algorithm>
+#include<queue>
 
 using namespace std;
+
+bool CheckImportance(queue<int> queue){
+    int checkNum = queue.front();
+    queue.pop();
+
+    for(int i = 0; i < queue.size(); i++){
+        int n = queue.front();
+        queue.pop();
+
+        if(n > checkNum) return false;
+    }
+    return true;
+}
 
 int main(){
     int testCase;
     cin >> testCase;
 
     for(int i = 0; i < testCase; i++){
-        int length = 0, index = 0, printCnt = 0;
-        cin >> length >> index;
+        int lenght, index, indexNum, count;
+        cin >> lenght >> index;
+        queue<int> printQue;
+        for(int j = 0; j < lenght; j++){
+            int n;
+            cin >> n;
+            if(j == index) indexNum = n;
+            printQue.push(n);
+        }
 
-        int* printQue = new int[length];
-
-        for(int j = 0; j < length; j++)
-            cin >> printQue[j];
-        
-        int* indexPoint = printQue + index;
-        sort(printQue, (printQue + length), greater<>());
-
-        for(int k = 0; k < length; k++){
-            printCnt++;
-            if(printQue + k == indexPoint){
-                cout << printCnt << "\n";
-                break;
+        while(!printQue.empty()){
+            if(CheckImportance(printQue)){
+                count++;
+                if(printQue.front() == indexNum){
+                    cout << count << "\n";
+                    break;
+                }
+                printQue.pop();
+            }
+            else{
+                printQue.push(printQue.front());
+                printQue.pop();
             }
         }
     }
