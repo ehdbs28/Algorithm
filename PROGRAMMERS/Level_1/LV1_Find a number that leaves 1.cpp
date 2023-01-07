a45381a9575624da38355b1d0464815432fc5759
도윤 <doyun5373@naver.com>
2023-01-07 20:50:04 +0900

    프로그래머스 레벨1 나머지가 1이 되는 수 찾기

--- PROGRAMMERS/Level_1/LV1_Find a number that leaves 1.cpp
+++ PROGRAMMERS/Level_1/LV1_Find a number that leaves 1.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+int solution(int n);
+
 int main(){
-    cout << "Hello World!";
+    cout << solution(12);
+}
+
+int solution(int n){
+    for(int i = 1; i < n; i++){
+        if(n % i == 1)
+            return i;
+    }
+
+    return 0;   
 }
