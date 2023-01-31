0e21b0791a166b0635a2a82b7465e1f919a869f4
도윤 <doyun5373@naver.com>
2023-01-31 20:10:13 +0900

    백준 2346 풍선 터트리기

--- BAEKJOON/Data_Structure/2346_Popping Balloon.cpp
+++ BAEKJOON/Data_Structure/2346_Popping Balloon.cpp
@@ -1,12 +1,38 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 
 using namespace std;
 
+bool check[1000] = {true, false};
+
+int FindAIndex(int max, int currentIndex, int value){
+    int checkValue = 0;
+    for(int i = ((value > 0) ? 1 : -1); ((value > 0) ? i <= value : i >= value); ((value > 0) ? i++ : i--)){
+        checkValue = currentIndex + i;
+
+        while(checkValue > max || checkValue <= 0){
+            if(checkValue > max)
+                checkValue -= max;
+            else if(checkValue <= 0)
+                checkValue = max - abs(checkValue);
+        }
+
+        if(check[checkValue - 1]){
+            currentIndex += (value > 0) ? 1 : -1;
+            (value > 0) ? --i : ++i;
+        }
+    }
+
+    check[checkValue - 1] = true;
+    return checkValue;
+}
+
 int main(){
+    //ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
     int ballonCount;
-    int index = 0;
-    int result[1000];
+    int index = 1;
 
     vector<int> ballonNums;
 
@@ -19,6 +45,9 @@ int main(){
     }
 
     for(int i = 0; i < ballonCount; i++){
-        
+        cout << index << " ";
+        if(i < ballonCount - 1)
+            index = FindAIndex(ballonCount, index, ballonNums[index - 1]);
     }
 }
+
