0e42fe4c5926bff90244f6a1e7a88aee249fd7c0
ehdbs28 <doyun5373@naver.com>
2023-04-17 19:44:17 +0900

    백준 1463 1로 만들기 ( 코드 수정 )

--- BAEKJOON/Basic/1463.cpp
+++ BAEKJOON/Basic/1463.cpp
@@ -11,18 +11,12 @@ int main(){
     cin >> num;
 
     for(int i = 2; i <= num; i++){
-        temp = 1 + dp[i - 1];
+        dp[i] = 1 + dp[i - 1];
 
-        if(i <= 3)
-            dp[i] = 1;
-        else if(i % 6 == 0)
-            dp[i] = min(min(1 + dp[i / 3], 1 + dp[i / 2]), temp);
-        else if(i % 3 == 0)
-            dp[i] = min(1 + dp[i / 3], temp);
-        else if(i % 2 == 0)
-            dp[i] = min(1 + dp[i / 2], temp);
-        else
-            dp[i] = temp;
+        if(i % 3 == 0)
+            dp[i] = min(1 + dp[i / 3], dp[i]);
+        if(i % 2 == 0)
+            dp[i] = min(1 + dp[i / 2], dp[i]);
     }
 
     cout << dp[num];
