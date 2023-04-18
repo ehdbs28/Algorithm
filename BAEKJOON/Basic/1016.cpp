de460fe58e44c2dd46f99bba4bb7a630ef622926
ehdbs28 <doyun5373@naver.com>
2023-04-19 08:29:08 +0900

    백준 1016 제곱 ㄴㄴ 수

---
+++ BAEKJOON/Basic/1016.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<climits>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    long long min, max, powNum, j;
+    long long array[1000001] = {};
+
+    int count = 0;
+
+    cin >> min >> max;
+
+    for(long long i = 2; i <= sqrt(max); i++){
+        powNum = i * i;
+        j = (min + powNum - 1) / powNum * powNum;
+
+        for( ; j <= max; j += powNum){
+            array[j - min] = 1;
+        }
+    }
+
+    for(long long i = min; i <= max; i++){
+        if(array[i - min] == 0){
+            ++count;
+        }
+    }
+
+    cout << count;
+}
