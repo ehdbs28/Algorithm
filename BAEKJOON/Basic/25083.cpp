c6211674f434dd6aeed880f487b33d6f6afcb2ce
ehdbs28 <doyun5373@naver.com>
2023-04-26 08:56:24 +0900

    백준 25083 새싹

---
+++ BAEKJOON/Basic/25083.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "         ,r\'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r\'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+}
