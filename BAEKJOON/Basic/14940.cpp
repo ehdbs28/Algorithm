59127b3cb7604d92de5134d68079b9f8f12b9d36
ehdbs28 <doyun5373@naver.com>
2023-06-28 08:32:11 +0900

    Update 14940.cpp

--- BAEKJOON/Basic/14940.cpp
+++ BAEKJOON/Basic/14940.cpp
@@ -2,27 +2,35 @@
 #include <algorithm>
 #include <utility>
 #include <queue>
-using namespace std;
 
+using namespace std;
 
 int board[1002][1002];
 int dist[1002][1002];
+
 int dx[4] = { 0,0,1,-1 };
 int dy[4] = { 1,-1,0,0 };
+
 queue<pair<int, int>> q;
+
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
 	int n, m;
+
 	cin >> n >> m;
+	
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			dist[i][j] = -1;
 		}
+	
 	}
+	
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> board[i][j];
+	
 			if (board[i][j] == 2) {
 				q.push({ i,j });
 				dist[i][j] = 0;
@@ -36,14 +44,15 @@ int main() {
 		for (int i = 0; i < 4; i++) {
 			int nx = cur.first + dx[i];
 			int ny = cur.second + dy[i];
+	
 			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
 			if (dist[nx][ny] != -1) continue;
 			if (board[nx][ny] == 0) continue;
+	
 			dist[nx][ny] = dist[cur.first][cur.second] + 1;
 			q.push({ nx,ny });
 		}
 	}
-
 	
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
