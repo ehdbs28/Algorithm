31939868321e854c4cf01278a3280c3330f760c9
doyun <98889991+ehdbs28@users.noreply.github.com>
2023-08-10 21:08:21 +0900

    백준 1197 최소 스패닝 트리

---
+++ BAEKJOON/Basic/1197.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+struct Edge{
+    int from;
+    int to;
+    int val;
+};
+
+int parent[10001] = {};
+vector<Edge> edges;
+
+int n;
+int m;
+
+int ans = 0;
+
+int Find(int x){
+    if(parent[x] == x)
+        return x;
+    return parent[x] = Find(parent[x]);
+}
+
+void merge(int x, int y){
+    x = Find(x);
+    y = Find(y);
+
+    if(x == y)
+        return;
+
+    parent[y] = x;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; i++){
+        parent[i] = i;
+    }
+
+    for(int i = 0; i < m; i++){
+        int from;
+        int to;
+        int val;
+        cin >> from >> to >> val;
+        edges.push_back({ from, to, val });
+    }
+
+    sort(edges.begin(), edges.end(), [](Edge a, Edge b){
+       return a.val < b.val;
+    });
+
+    for(int i = 0; i < m; i++){
+        Edge e = edges[i];
+
+        if(Find(e.from) == Find(e.to)){
+            continue;
+        }
+
+        merge(e.from, e.to);
+        ans += e.val;
+    }
+
+    cout << ans;
+}
