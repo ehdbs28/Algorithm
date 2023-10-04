6b643b78473ccad70bb0322ab983a3ce950f1c2a
ehdbs28 <doyun5373@naver.com>
2023-10-05 08:46:58 +0900

    백준 11000 강의실 배정

---
+++ BAEKJOON/Basic/11000.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    int ans = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> rooms;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int s, e;
+        cin >> s >> e;
+        pq.emplace(s, e);
+    }
+
+    while(!pq.empty()){
+        int s = pq.top().first;
+        int e = pq.top().second;
+        pq.pop();
+
+        if(!rooms.empty() && rooms.top().first <= s){
+            rooms.pop();
+            rooms.emplace(e, s);
+        }
+        else{
+            ans++;
+            rooms.emplace(e, s);
+        }
+    }
+
+    cout << ans;
+}
